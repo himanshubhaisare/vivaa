3a9868f92da78ab153808228eedd51eb99016299
Gurugulaab <himanshu.bhaisare@gmail.com>
2018-05-05 17:08:10 -0700

    semicolons

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var express = require('express')
+var express = require('express');
 var database = require('./database');
 
-var app = express()
+var app = express();
 
 app.get('/', function (req, res) {
     res.send('Vivaa');
@@ -12,10 +12,10 @@ app.get('/physicians', function (req, res) {
 });
 
 app.get('/physicians/appointments', function (req, res) {
-    res.send(database.appointments)
+    res.send(database.appointments);
 });
 
 if (!module.parent) {
-    app.listen(3000);
+    app.listen(4000);
     console.log('Express started on port 3000');
 }
