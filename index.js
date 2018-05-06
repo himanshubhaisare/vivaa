41d3a3ac1a98ab3b96a9b2e78561241331ea71cb
Gurugulaab <himanshu.bhaisare@gmail.com>
2018-05-05 17:05:47 -0700

    tried adding babel, took too long, not doing it.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express')
+var database = require('./database');
+
 var app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.send('Vivaa');
+});
+
+app.get('/physicians', function (req, res) {
+    res.send(database.physicians);
+});
+
+app.get('/physicians/appointments', function (req, res) {
+    res.send(database.appointments)
 });
 
 if (!module.parent) {
