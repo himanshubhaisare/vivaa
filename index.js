55f2ae56d2b3a6e3b7eb4cdda62336c2d4e8ec31
Gurugulaab <himanshu.bhaisare@gmail.com>
2018-05-05 19:24:16 -0700

    handle CORS

--- index.js
+++ index.js
@@ -6,19 +6,21 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Vivaa');
 });
-
-app.get('/physicians', function (req, res) {
-    res.set({
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.set({
         'Content-Type': 'application/json',
     });
+  next();
+});
+
+app.get('/physicians', function (req, res) {
     res.status(200)
     res.json(database.physicians);
 });
 
 app.get('/physicians/appointments', function (req, res) {
-    res.set({
-        'Content-Type': 'application/json',
-    });
     res.status(200);
     res.json(database.appointments);
 });
